48539b2f026856396c62801757e41b6e18a47018
Uty KKan <Uty KKan@DESKTOP-C2MEFB8>
2020-08-31 17:06:13 +0700

    End

--- Week4_1/Source.cpp
+++ Week4_1/Source.cpp
@@ -1,36 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main() {
-	int x;
-	scanf("%d", &x);
-	if (x < 0)
+	int num;
+	scanf("%d", &num);
+	if (num < 0)
 	{
 		printf("PLease insert the number that is greater or equal zero");
 		return 0;
 	}
-	else if (x >= 0)
+	else if (num >= 0)
 	{
-		if (x > 80)
+		if (num >= 80)
 		{
 			printf("A");
 			return 0;
 		}
-		else if (x >= 70)
+		else if (num >= 70)
 		{
 			printf("B");
 			return 0;
 		}
-		else if (x >= 60)
+		else if (num >= 60)
 		{
 			printf("C");
 			return 0;
 		}
-		else if (x >= 50)
+		else if (num >= 50)
 		{
 			printf("D");
 			return 0;
 		}
-		else if (x < 50)
+		else if (num < 50)
 		{
 			printf("F");
 			return 0;
